e39c91c8c049a6e18f586748b23aa4a08c267b7b
William Nak <william.nak@etud.polymtl.ca>
2026-01-27 19:05:41 -0500

    added app files

---
+++ app/schemas.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from enum import Enum
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class UserBaseSchema(BaseModel):
+
+    id: UUID | None = None
+    first_name: str = Field(
+        ..., description="The first name of the user", example="John"
+    )
+    last_name: str = Field(..., description="The last name of the user", example="Doe")
+    address: str | None = None
+    activated: bool = False
+    createdAt: datetime | None = None
+    updatedAt: datetime | None = None
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
+        arbitrary_types_allowed = True
+
+
+class Status(Enum):
+    Success = "Success"
+    Failed = "Failed"
+
+
+class UserResponse(BaseModel):
+    Status: Status
+    User: UserBaseSchema
+
+
+class GetUserResponse(BaseModel):
+    Status: Status
+    User: UserBaseSchema
+
+
+class ListUserResponse(BaseModel):
+    status: Status
+    results: int
+    users: list[UserBaseSchema]
+
+
+class DeleteUserResponse(BaseModel):
+    Status: Status
+    Message: str
